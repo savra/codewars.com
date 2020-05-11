ff24e506ed22e598705eaecee187ab0101c69eb2
Sergey Savrukhin <savrawar@mail.ru>
2020-05-11 20:19:37 +0300

    Return the number (count) of vowels in the given string.
    We will consider a, e, i, o, and u as vowels for this Kata.
    
    The input string will only consist of lower case letters and/or spaces.

--- src/src/Vowels/Vowels.java
+++ src/src/Vowels/Vowels.java
@@ -1,7 +1,18 @@
 package Vowels;
 
-public class Vowels {
-    public static void main(String[] args) {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class Vowels {
+    public static int getCount(String str) {
+        int vowelsCount;
+
+        vowelsCount = Pattern.compile("[^aeiou]").matcher(str).replaceAll("").length();
+
+        return vowelsCount;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Vowels.getCount("Nopuaeae!"));
     }
 }
