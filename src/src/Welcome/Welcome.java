7ec738a9a9463eb21411d4a234880619bfd6ebda
Sergey Savrukhin <savrawar@mail.ru>
2020-05-23 12:19:10 +0300

    Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
    The Task
    Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
    Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
    The Database
    english: 'Welcome',
    czech: 'Vitejte',
    danish: 'Velkomst',
    dutch: 'Welkom',
    estonian: 'Tere tulemast',
    finnish: 'Tervetuloa',
    flemish: 'Welgekomen',
    french: 'Bienvenue',
    german: 'Willkommen',
    irish: 'Failte',
    italian: 'Benvenuto',
    latvian: 'Gaidits',
    lithuanian: 'Laukiamas',
    polish: 'Witamy',
    spanish: 'Bienvenido',
    swedish: 'Valkommen',
    welsh: 'Croeso'
    Possible invalid inputs include:
    
    IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
    IP_ADDRESS_NOT_FOUND - ip address not in the database
    IP_ADDRESS_REQUIRED - no ip address was supplied

---
+++ src/src/Welcome/Welcome.java
@@ -0,0 +1,42 @@
+package Welcome;
+
+public class Welcome {
+    public static String greet(String language) {
+
+        return switch (language) {
+            case "english" -> "Welcome";
+            case "czech" -> "Vitejte";
+            case "danish" -> "Velkomst";
+            case "dutch" -> "Welkom";
+            case "estonian" -> "Tere tulemast";
+            case "finnish" -> "Tervetuloa";
+            case "flemish" -> "Welgekomen";
+            case "french" -> "Bienvenue";
+            case "german" -> "Willkommen";
+            case "irish" -> "Failte";
+            case "italian" -> "Benvenuto";
+            case "latvian" -> "Gaidits";
+            case "lithuanian" -> "Laukiamas";
+            case "polish" -> "Witamy";
+            case "spanish" -> "Bienvenido";
+            case "swedish" -> "Valkommen";
+            case "welsh" -> "Croeso";
+            default -> "Welcome";
+        };
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
