37689d64c60c029a3fef789b83fa380c79832143
Sergey Savrukhin <savrawar@mail.ru>
2022-03-29 20:46:53 +0300

    easy logs

--- src/javatask/ZywOo.java
+++ src/javatask/ZywOo.java
@@ -1,7 +1,5 @@
 package javatask;
 
-import java.util.Arrays;
-
 public class ZywOo {
     public static int sumOfDifferences(int[] arr) {
         if (arr.length <= 1) return 0;
@@ -26,10 +24,4 @@ public class ZywOo {
 
         return result;
     }
-
-    public static void main(String[] args) {
-        sumOfDifferences(new int[]{8, 7, 11, 13, 1, 9, 5,3,2,2,4,5,6,6});
-    }
 }
-//4
-//13
