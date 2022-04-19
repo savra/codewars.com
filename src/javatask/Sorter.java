d90ca2d22a553e79e116bf63cc7ff444cf59dc6a
Sergey Savrukhin <savra.sv@yandex.ru>
2022-04-19 19:32:12 +0300

    Sort My Textbooks

---
+++ src/javatask/Sorter.java
@@ -0,0 +1,10 @@
+package javatask;
+
+import java.util.List;
+
+public class Sorter {
+    public static List<String> sort(List<String> textbooks) {
+        textbooks.sort(String.CASE_INSENSITIVE_ORDER);
+        return textbooks;
+    }
+}
