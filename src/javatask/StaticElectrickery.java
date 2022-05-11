ede6b00b3e13ac2059e3a40236c0a2795afa9ef3
Sergey Savrukhin <savra.sv@yandex.ru>
2022-05-11 21:09:58 +0300

    FIXME: Static electrickery

---
+++ src/javatask/StaticElectrickery.java
@@ -0,0 +1,21 @@
+package javatask;
+
+public class StaticElectrickery {
+    private static int ONE_HUNDRED = 100;
+
+    public static final StaticElectrickery INST = new StaticElectrickery();
+
+    private final int value;
+
+    private StaticElectrickery() {
+        value = ONE_HUNDRED;
+    }
+
+    public int plus100(int n) {
+        return value + n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StaticElectrickery.INST.plus100(23));
+    }
+}
