36e4762663670f69398ef4dd613387df30573d97
Sergey Savrukhin <savra.sv@yandex.ru>
2022-05-24 23:18:18 +0300

    Volume of a Cuboid

---
+++ src/main/java/com/hvdbs/codewars/javatask/VolumeOfCuboid.java
@@ -0,0 +1,7 @@
+package com.hvdbs.codewars.javatask;
+
+public class VolumeOfCuboid {
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        return length * width * height;
+    }
+}
