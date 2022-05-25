9a8faea0476b0901220fd2d0ef1cf2b67b73ffe0
Sergey Savrukhin <savra.sv@yandex.ru>
2022-05-26 00:05:58 +0300

    KeepHydrated

--- src/main/java/com/hvdbs/codewars/javatask/KeepHydrated.java
+++ src/main/java/com/hvdbs/codewars/javatask/KeepHydrated.java
@@ -1,2 +1,7 @@
-package com.hvdbs.codewars.javatask;public class KeepHydrated {
+package com.hvdbs.codewars.javatask;
+
+public class KeepHydrated {
+    public int Liters(double time)  {
+        return (int)Math.floor(time * 0.5);
+    }
 }
