5becb26ffdf6cd078ab95268e769592f8ae275a6
Sergey Savrukhin <savra.sv@yandex.ru>
2022-05-26 20:43:45 +0300

    Thinkful - Number Drills: Pixelart planning

---
+++ src/main/java/com/hvdbs/codewars/javatask/PixelartPlanning.java
@@ -0,0 +1,7 @@
+package com.hvdbs.codewars.javatask;
+
+public class PixelartPlanning {
+    public static boolean isDivisible(int wallLength, int pixelSize) {
+        return wallLength % pixelSize == 0;
+    }
+}
