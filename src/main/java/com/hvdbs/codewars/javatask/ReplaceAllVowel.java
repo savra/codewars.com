79439f15b7ce4d36ce40cd5f4318bdfbc9aabd80
Sergey Savrukhin <savra.sv@yandex.ru>
2022-06-06 00:00:38 +0300

    Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence

--- src/main/java/com/hvdbs/codewars/javatask/ReplaceAllVowel.java
+++ src/main/java/com/hvdbs/codewars/javatask/ReplaceAllVowel.java
@@ -1,5 +1,9 @@
 package com.hvdbs.codewars.javatask;
 
-public class ReplaceAllVowel {
+import java.util.regex.Pattern;
 
+public class ReplaceAllVowel {
+    public static String replace(final String s) {
+        return Pattern.compile("[aeiou]", Pattern.CASE_INSENSITIVE).matcher(s).replaceAll("!");
+    }
 }
