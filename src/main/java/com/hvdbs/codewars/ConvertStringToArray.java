777a8ba7a5bb9dd7dd67bb0770d7ce1513c3b9c0
Sergey Savrukhin <savra.sv@yandex.ru>
2022-07-03 19:17:28 +0300

    Convert a string to an array

---
+++ src/main/java/com/hvdbs/codewars/ConvertStringToArray.java
@@ -0,0 +1,9 @@
+package com.hvdbs.codewars;
+
+import java.util.Arrays;
+
+public class ConvertStringToArray {
+    public static String[] stringToArray(String s) {
+        return s.split(" ");
+    }
+}
