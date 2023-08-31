e6610625e873cafed0f7d06b6fe9a868c123cb6d
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-31 20:20:03 +0300

    Exclusive "or" (xor) Logical Operator

--- src/main/java/com/hvdbs/codewars/javatask/ExclusiveOrXorLogicalOperator.java
+++ src/main/java/com/hvdbs/codewars/javatask/ExclusiveOrXorLogicalOperator.java
@@ -9,6 +9,8 @@ import com.hvdbs.codewars.statgenerator.enums.Difficulty;
         url = "https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c"
 )
 public class ExclusiveOrXorLogicalOperator {
+    //My Time Complexity: O(1)
+    //My Space complexity: O(1)
     public static boolean xor(boolean a, boolean b) {
         return a ^ b;
     }
