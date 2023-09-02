613b9ddb15de42a83c30e8a693c94098685662ac
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-02 07:23:06 +0300

    Rock Paper Scissors!

--- src/main/java/com/hvdbs/codewars/Main.java
+++ src/main/java/com/hvdbs/codewars/Main.java
@@ -9,6 +9,5 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
         StatsGenerator.generate();
-        System.out.println("GENERATED!!!");
     }
 }
