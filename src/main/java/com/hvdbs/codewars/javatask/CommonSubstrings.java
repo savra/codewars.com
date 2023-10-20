a691acca2f292a2ad42707073edd255de8264a3f
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-20 09:28:21 +0300

    Common Substrings

---
+++ src/main/java/com/hvdbs/codewars/javatask/CommonSubstrings.java
@@ -0,0 +1,28 @@
+package com.hvdbs.codewars.javatask;
+
+import com.hvdbs.codewars.statgenerator.CodewarsInfo;
+import com.hvdbs.codewars.statgenerator.enums.Difficulty;
+
+@CodewarsInfo(
+        difficulty = Difficulty.SEVEN,
+        name = "Common Substrings",
+        url = "https://www.codewars.com/kata/5669a5113c8ebf16ed00004c"
+)
+public class CommonSubstrings {
+    public static boolean SubstringTest(String str1, String str2) {
+        if (str1 == null || str2 == null) {
+            return false;
+        }
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        for (int i = 0; i < str1.length() - 2; i++) {
+            if (str2.contains(str1.substring(i, i + 2))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
