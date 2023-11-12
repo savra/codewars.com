1d78efd7dbab91638bc8fc5c198e0cc47cc5843c
Sergey Savrukhin <savra.sv@yandex.ru>
2023-11-12 17:57:28 +0300

    Closing in Sum

--- src/main/java/com/hvdbs/codewars/javatask/ClosingInSum.java
+++ src/main/java/com/hvdbs/codewars/javatask/ClosingInSum.java
@@ -14,6 +14,7 @@ public class ClosingInSum {
         int[] tmp = new int[(int) Math.log10(n) + 1];
 
         int i = 0;
+
         int sum = 0;
 
         while (n > 0) {
@@ -36,3 +37,5 @@ public class ClosingInSum {
         closingInSum(1039L);
     }
 }
+
+
