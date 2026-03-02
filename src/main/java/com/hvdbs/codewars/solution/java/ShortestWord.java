e2ee37f956de0820e25b08671fc2e05b8af236ee
Sergey Savrukhin <savra.sv@yandex.ru>
2026-03-02 21:51:00 +0300

    Shortest Word

--- src/main/java/com/hvdbs/codewars/solution/java/ShortestWord.java
+++ src/main/java/com/hvdbs/codewars/solution/java/ShortestWord.java
@@ -1,16 +1,29 @@
 package com.hvdbs.codewars.solution.java;
 
 import com.hvdbs.savra.statsgenerator.CodeInfo;
+import com.hvdbs.savra.statsgenerator.enums.Complexity;
 import com.hvdbs.savra.statsgenerator.enums.Difficulty;
-
-import java.util.Arrays;
 
 @CodeInfo(
         difficulty = Difficulty.SEVEN,
         name = "Shortest Word",
-        url = "https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9")
+        url = "https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9",
+        timeComplexity = Complexity.ConstantComplexity.LINEAR,
+        spaceComplexity = Complexity.ConstantComplexity.CONSTANT)
 public class ShortestWord {
     public static int findShort(String s) {
-        return Arrays.stream(s.split(" ")).mapToInt(String::length).min().orElse(-1);
+        int min = 0x7fffffff;
+        int curMin = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                min = Math.min(curMin, min);
+                curMin = 0;
+            } else {
+                curMin++;
+            }
+        }
+
+        return Math.min(curMin, min);
     }
 }
